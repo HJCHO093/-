c1ec82d7fbff075d84a5784c676036c3f1ec8a15
안용상 <dydtkddhkdwk@naver.com>
2023-04-27 00:24:07 +0900

    stave width 인식 threshold 값 0.5 -> 0.8로 변경

--- modules.py
+++ modules.py
@@ -27,7 +27,7 @@ def remove_staves(image): #removing staves
         pixels=0
         for col in range(width):
             pixels += (image[row][col]==255) #counting white pixels in each row
-        if pixels >= width * 0.5: #adding staves
+        if pixels >= width * 0.8: #adding staves #width에 곱해지는 실수값이 stave를 인식하기 위해 중요
             if len(staves)==0 or abs(staves[-1][0]+staves[-1][1]-row)>1:
                 staves.append([row, 0])
             else:
